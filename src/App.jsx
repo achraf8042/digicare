dd840f473164715e0d621e22cb32a36e5ed1f4df
salmyachraf <salmyachraf@gmail.com>
2025-12-07 19:37:12 +0100

    fix issue

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import PrivacyPolicy from './components/PrivacyPolicy'
 import License from './components/License'
 import Marketing from './components/Marketing'
 import ScrollToTop from './components/ScrollToTop'
+import Welcome from './components/Welcome'
 
 const Home = () => (
   <>
